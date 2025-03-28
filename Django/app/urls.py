7be18acbeb80f06892b9924c04a4eb1a1795f642
kashishrajputt <btecg41522029@dseu.ac.in>
2025-03-29 02:13:42 +0530

    creating an app

---
+++ Django/app/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+#localhost:8000/app
+urlpatterns = [
+    
+    path('', views.all_app, name ='all_app'),
+    
+]
--- Django/app/views.py
+++ Django/app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def all_app(request):
+    return render(request,'app/all_app.html')
