79fb0654bcdb7777e51e562f99646f4caf9dc6f9
kashishrajputt <btecg41522029@dseu.ac.in>
2025-03-30 01:44:15 +0530

    admin panel

--- Django/Django/urls.py
+++ Django/Django/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('about/', views.about, name = 'about'),
     path('contact/', views.contact, name ='contact'),
     path('app/', include('app.urls')),
+
+    path("__reload__/", include("django_browser_reload.urls")),#always at last
 ]
